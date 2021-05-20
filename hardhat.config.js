7f8cdf88e70b035443f5816b618e2523120caa52
Jens Ivar Jordre <jensivar@nahmii.io>
2021-05-20 09:31:01 +0200

    Update solidity and network settings
    Add optimization to solidity settings.
    
    Add mainnet network configuration. Update Ropsten network configuration. In both cases
    default to accounts by mnemonic if no private key is provided as env var.

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,29 @@ const { etherscanApiKey, mnemonic } = require('./secrets.json');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     hardhat: {},
     localhost: {},
+    mainnet: {
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : { mnemonic },
+      gasPrice: 130000000000,
+      timeout: 300000,
+      url: 'https://ethereum.hubii.com',
+    },
     ropsten: {
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : { mnemonic },
+      gasPrice: 80000000000,
+      timeout: 120000,
       url: 'https://geth-ropsten.dev.hubii.net', // `https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}`
-      accounts: { mnemonic },
     },
   },
   etherscan: {
